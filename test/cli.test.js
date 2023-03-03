3eb937baadb003f419db14f5e474bbaa41a21a48
Marc Lynch <lynchmarc2@gmail.com>
2023-03-03 16:21:39 -0600

    refactor: esm

--- test/cli.test.js
+++ test/cli.test.js
@@ -8,41 +8,42 @@
  * TypeScript should NOT be configured by default in this package.
  * This package should mimic the user experience as closely as possible.
  */
-
-import { describe, expect, test } from 'vitest';
-import { commandSync } from 'execa';
+import Module from "node:module";
+import { describe, expect, test } from "vitest";
+import { commandSync } from "execa";
 // This binary is coming from the "download-rehearsal-cli" script
 // we want to test the CLI binary that is downloaded from the master branch
 // of https://github.com/rehearsal-js/rehearsal-js
 // and NOT the @rehearsal/cli version published to npm
-const CLI_BIN = require.resolve('@rehearsal/cli/bin/rehearsal.js');
+const require = Module.createRequire(import.meta.url);
+const CLI_BIN = require.resolve("@rehearsal/cli/bin/rehearsal.js");
 
 const run = (args, options) => {
-  return commandSync(`node ${CLI_BIN} ${args.join(' ')}`, options);
+  return commandSync(`node ${CLI_BIN} ${args.join(" ")}`, options);
 };
 
-describe('smoke-test @rehearsal/cli', () => {
-  test('without command only --help', () => {
-    const results = run(['--help']);
+describe("smoke-test @rehearsal/cli", () => {
+  test("without command only --help", () => {
+    const results = run(["--help"]);
     expect(results.exitCode).toBe(0);
-    expect(results.stdout).toContain('Usage: rehearsal [options] [command]');
-    expect(results.stdout).toContain('migrate [options]');
-    expect(results.stdout).toContain('upgrade [options] [basePath]');
+    expect(results.stdout).toContain("Usage: rehearsal [options] [command]");
+    expect(results.stdout).toContain("migrate [options]");
+    expect(results.stdout).toContain("upgrade [options] [basePath]");
   });
 });
 
-describe('smoke-test @rehearsal/cli migrate', () => {
-  test('migrate command --help', () => {
-    const results = run(['migrate', '--help']);
+describe("smoke-test @rehearsal/cli migrate", () => {
+  test("migrate command --help", () => {
+    const results = run(["migrate", "--help"]);
     expect(results.exitCode).toBe(0);
-    expect(results.stdout).toContain('migrate [options]');
+    expect(results.stdout).toContain("migrate [options]");
   });
 });
 
-describe('smoke-test @rehearsal/cli upgrade', () => {
-  test('upgrade command --help', () => {
-    const results = run(['upgrade', '--help']);
+describe("smoke-test @rehearsal/cli upgrade", () => {
+  test("upgrade command --help", () => {
+    const results = run(["upgrade", "--help"]);
     expect(results.exitCode).toBe(0);
-    expect(results.stdout).toContain('upgrade [options] [basePath]');
+    expect(results.stdout).toContain("upgrade [options] [basePath]");
   });
 });
